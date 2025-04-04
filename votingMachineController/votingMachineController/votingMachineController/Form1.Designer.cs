2c9eb7755cb081634a2e363a83a3d55bf8f1f8ae
Guilherme731 <gui.pilz.2007@gmail.com>
2025-04-04 19:15:29 -0300

    Created basic UI for simple serial communication

---
+++ votingMachineController/votingMachineController/votingMachineController/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace votingMachineController
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.cbSelectCOM = new System.Windows.Forms.ComboBox();
+            this.btnSendMessage = new System.Windows.Forms.Button();
+            this.timerCOM = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.tbSendMessage = new System.Windows.Forms.TextBox();
+            this.tbConsole = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.Location = new System.Drawing.Point(233, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(169, 34);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            // 
+            // cbSelectCOM
+            // 
+            this.cbSelectCOM.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbSelectCOM.FormattingEnabled = true;
+            this.cbSelectCOM.Location = new System.Drawing.Point(12, 12);
+            this.cbSelectCOM.Name = "cbSelectCOM";
+            this.cbSelectCOM.Size = new System.Drawing.Size(215, 33);
+            this.cbSelectCOM.TabIndex = 1;
+            // 
+            // btnSendMessage
+            // 
+            this.btnSendMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSendMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSendMessage.Location = new System.Drawing.Point(806, 49);
+            this.btnSendMessage.Name = "btnSendMessage";
+            this.btnSendMessage.Size = new System.Drawing.Size(169, 32);
+            this.btnSendMessage.TabIndex = 2;
+            this.btnSendMessage.Text = "Send";
+            this.btnSendMessage.UseVisualStyleBackColor = true;
+            // 
+            // timerCOM
+            // 
+            this.timerCOM.Interval = 1000;
+            // 
+            // tbSendMessage
+            // 
+            this.tbSendMessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbSendMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbSendMessage.Location = new System.Drawing.Point(12, 51);
+            this.tbSendMessage.Name = "tbSendMessage";
+            this.tbSendMessage.Size = new System.Drawing.Size(788, 30);
+            this.tbSendMessage.TabIndex = 3;
+            // 
+            // tbConsole
+            // 
+            this.tbConsole.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbConsole.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.tbConsole.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbConsole.ForeColor = System.Drawing.Color.LimeGreen;
+            this.tbConsole.Location = new System.Drawing.Point(13, 88);
+            this.tbConsole.Multiline = true;
+            this.tbConsole.Name = "tbConsole";
+            this.tbConsole.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbConsole.Size = new System.Drawing.Size(962, 448);
+            this.tbConsole.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.BlueViolet;
+            this.ClientSize = new System.Drawing.Size(987, 548);
+            this.Controls.Add(this.tbConsole);
+            this.Controls.Add(this.tbSendMessage);
+            this.Controls.Add(this.btnSendMessage);
+            this.Controls.Add(this.cbSelectCOM);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "Voting Machine Controller - Disconected";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ComboBox cbSelectCOM;
+        private System.Windows.Forms.Button btnSendMessage;
+        private System.Windows.Forms.Timer timerCOM;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox tbSendMessage;
+        private System.Windows.Forms.TextBox tbConsole;
+    }
+}
+
---
+++ votingMachineController/votingMachineController/votingMachineController/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace votingMachineController
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateCOMList()
+        {
+            int i = 0;
+            bool changed = false;
+
+            if (cbSelectCOM.Items.Count == SerialPort.GetPortNames().Length)
+            {
+                foreach (string s in SerialPort.GetPortNames())
+                {
+                    if (cbSelectCOM.Items[i++].Equals(s) == false)
+                    {
+                        changed = true;
+                    }
+                }
+            }
+            else
+            {
+                changed = true;
+            }
+
+            if (!changed)
+            {
+                return;
+            }
+
+            cbSelectCOM.Items.Clear();
+
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                cbSelectCOM.Items.Add(s);
+            }
+            //seleciona a primeira posição da lista
+            cbSelectCOM.SelectedIndex = 0;
+        }
+    }
+}
